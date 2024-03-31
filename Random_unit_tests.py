c98620f0e460cfa1e3f83f684de9d610689f9cea
Deshilan <janakadeshi@gmail.com>
2024-03-31 23:34:47 +0200

    Update

--- Random_unit_tests.py
+++ Random_unit_tests.py
@@ -28,7 +28,18 @@ def AWS_AWI_check(raw_json):
     return result
 
 f = open("test.json")
+file = json.load(f)
 for m in range(1000):
     x = random.randint(1,1000)
+    wanted_result = True
     word = ''.join(random.choices(string.ascii_uppercase + string.digits, k=x))
+    if word == "*":
+        wanted_result = False
+    file['PolicyDocument']['Statement'][0]["Resource"] = word
+    json_update = open("test.json", "w+")
+    json_update.write(json.dumps(file))
+    json_update.close()
+    f = open("test.json")
+    AWS_AWI_check(f)
+
 
